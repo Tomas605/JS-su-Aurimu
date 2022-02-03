d954dc677d8956dc3f9e7e37bfcc9293bbd5166f
Tomas Uzpurvis <tomas605@gmail.com>
2022-02-03 19:41:46 +0200

    Trecia uzduotis

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const puodelioKaina = 1.5;
 const puodeliuKiekis = prompt("Kiek puodeliu kavos?");
 let suma = 0;
 
-for (let i = 0; i < puodeliuKiekis; i++) {
-    suma = suma + puodelioKaina;
+for (let cup = 1; cup <= puodeliuKiekis; cup++) {
+    const cukrausKiekis = prompt(`Kiek sauksteliu cukraus ${cup} puodelyje?`);
+
+    for (let cukrus = 1; cukrus <= cukrausKiekis; cukrus++) {
+        const sakinys = `Pilu ${cukrus} sauksteli cukraus i ${cup} puodeli`;
     }
 
-alert(`Suma ${suma}`);
+    const sakinys = `Pilu 1 sauksteli cukraus i ${cup} puodeli`;
+
+    suma = suma + puodelioKaina;
+}
+
+alert(`Suma ${suma} eur`);
