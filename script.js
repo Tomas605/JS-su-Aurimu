1fa217af866c8b2c7cc9c31454aada1dd030b6c0
Tomas Uzpurvis <tomas605@gmail.com>
2022-02-05 09:46:04 +0200

    1 Uzduotis

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
+const vardas ="Tomas Uzpurvis" ;
 
-const puodelioKaina = 1.5;
-const puodeliuKiekis = prompt("Kiek puodeliu kavos?");
-let suma = 0;
+const raidziuSkaicius = vardas.length;
 
-for (let cup = 1; cup <= puodeliuKiekis; cup++) {
-    const cukrausKiekis = prompt(`Kiek sauksteliu cukraus ${cup} puodelyje?`);
+const fullName = vardas.split(" "); // dalina varda su pavarde
 
-    for (let cukrus = 1; cukrus <= cukrausKiekis; cukrus++) {
-        const sakinys = `Pilu ${cukrus} sauksteli cukraus i ${cup} puodeli`;
-    }
+const vardasMazosiomis = vardas.toLowerCase();// mazosiomis raidemis 
+const vardasDidziosiomis = vardas.toUpperCase();// didziosiomis raidemis 
+const apkeistaRaide = vardasMazosiomis.replace(/s/g, "");// regular expressions 
 
-    const sakinys = `Pilu 1 sauksteli cukraus i ${cup} puodeli`;
-
-    suma = suma + puodelioKaina;
-}
-
-alert(`Suma ${suma} eur`);
+console.log(apkeistaRaide);
