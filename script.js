724c2b90cf5c1cc1490a4e1c87138f5cb453c795
Tomas Uzpurvis <tomas605@gmail.com>
2022-02-06 09:09:33 +0200

    Antra uzduotis

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const vardas ="Tomas Uzpurvis" ;
+/*const vardas ="Tomas Uzpurvis" ;
 
 const raidziuSkaicius = vardas.length;
 
@@ -8,4 +8,37 @@ const vardasMazosiomis = vardas.toLowerCase();// mazosiomis raidemis
 const vardasDidziosiomis = vardas.toUpperCase();// didziosiomis raidemis 
 const apkeistaRaide = vardasMazosiomis.replace(/s/g, "");// regular expressions 
 
-console.log(apkeistaRaide);
+console.log(apkeistaRaide);*/
+
+const transliacija = `
+ 32   36   44 16  22      18     8   30   12      86   44
+  54   10  64       6         60    84         2       98
+40 20     32  44   10        4   172       4   108
+     12 120 168    0 10   24        4 196     44   9 188 4
+ 7 16    18  22    8    11 9   4 15 6 43 22
+27 5       32              3 30 42 1       49 20
+
+10     16    22      5  2 86
+2 54    6    60 84  0   5    12      2  98 22 40     74 4
+`;
+
+const transliacijaBeNewLine = transliacija.replaceAll('\n', ' ');
+const masyvasSuTarpais = transliacijaBeNewLine.split(' ');
+const skaiciuMasyvas = masyvoValytuvas(masyvasSuTarpais);
+const atkirptiSkaiciuMasyvas = skaiciuMasyvas.slice(-40);
+
+console.log(atkirptiSkaiciuMasyvas);
+
+function masyvoValytuvas(masyvasKuriReikiaIsvalyt) {
+  const tikSkaiciai = [];
+
+  for (let i = 0; i < masyvasKuriReikiaIsvalyt.length; i++) {
+    if (masyvasKuriReikiaIsvalyt[i] === '') {
+      //nedarom nieko
+    } else {
+      tikSkaiciai.push(masyvasKuriReikiaIsvalyt[i]);
+    }
+  }
+
+  return tikSkaiciai;
+}
